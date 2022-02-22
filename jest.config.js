bca755d095b2c5046e91e1bee3c0e36b2d19dce1
Diogo Ferreira <diogocraft.2@gmail.com>
2022-02-21 22:24:13 -0300

    :wrench: added tweaks to jest settings

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,30 @@
 module.exports = {
   roots: ['<rootDir>/tests'],
+  testMatch: [
+    '<rootDir>/tests/**/*.spec.ts',
+    '<rootDir>/tests/**/*.test.ts'
+  ],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!**/node_modules/**',
+    '!**/tests/**',
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
-  preset: '@shelf/jest-mongodb',
+  clearMocks: true,
+  // preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
   moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1'
+    '@data/(.*)': '<rootDir>/src/data/$1',
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1',
+    '@core/(.*)': '<rootDir>/src/core/$1',
+    '@presentation/(.*)': '<rootDir>/src/presentation/$1',
+    '@validation/(.*)': '<rootDir>/src/validation/$1',
+    '@tests/(.*)': '<rootDir>/tests/$1'
   }
 }
